7cbb2a452e4708337f89aa256f9a34579ef66dab
Shyamsundar <sradarapu37397@ucumberlands.edu>
2026-07-29 22:54:04 -0500

    Adding test case for test matrix

--- tests/test_data_structures.py
+++ tests/test_data_structures.py
@@ -6,6 +6,7 @@ from part2_data_structures.dynamic_array import DynamicArray
 from part2_data_structures.stack import Stack
 from part2_data_structures.queue import Queue
 from part2_data_structures.linked_list import SinglyLinkedList
+from part2_data_structures.matrix import Matrix
 
 
 class TestDynamicArray(unittest.TestCase):
@@ -68,6 +69,27 @@ class TestLinkedList(unittest.TestCase):
         self.assertFalse(linked.search(20))
         self.assertEqual(linked.traverse(), [10, 30])
 
+class TestMatrix(unittest.TestCase):
+
+    def test_access_update_insert_and_delete(self):
+        matrix = Matrix(2, 2)
+
+        matrix.set(0, 0, 10)
+        matrix.set(1, 1, 20)
+
+        self.assertEqual(matrix.get(0, 0), 10)
+        self.assertEqual(matrix.get(1, 1), 20)
+
+        matrix.insert_row(1, [30, 40])
+
+        self.assertEqual(matrix.rows, 3)
+        self.assertEqual(matrix.get(1, 0), 30)
+        self.assertEqual(matrix.get(1, 1), 40)
+
+        matrix.delete_row(0)
+
+        self.assertEqual(matrix.rows, 2)
+        self.assertEqual(matrix.get(0, 0), 30)
 
 if __name__ == "__main__":
     unittest.main()
